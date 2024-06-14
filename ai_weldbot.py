ae2c71f36af0898bbe35eb81c288f6c97d9d0b37
Demkov Boris <demkov.boris@gmail.com>
2024-06-14 17:01:21 +0300

    work telegram bot with st yolov5 model

--- ai_weldbot.py
+++ ai_weldbot.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import shutil
 from pathlib import Path
 
+import torch
 from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
@@ -16,39 +18,38 @@ CHAT_ID = os.getenv('CHAT_ID')
 
 DEBUG = os.getenv('DEBUG', default='False') == 'True'
 
-# bot = Bot(token=BOT_TOKEN)
-# # Укажите id своего аккаунта в Telegram
-# chat_id = CHAT_ID
-# text = 'Вам телеграмма!'
-# # Отправка сообщения
-# bot.send_message(chat_id, text)
-
-# def get_new_image():
-#     try:
-#         response = requests.get(URL)
-#     except Exception as error:
-#         print(error)
-#         new_url = 'https://api.thedogapi.com/v1/images/search'
-#         response = requests.get(new_url)
-
-
-#     response = response.json()
-#     random_cat = response[0].get('url')
-#     return random_cat
-
 
 def get_image_predict(photo_weld):
-    # f = BytesIO(file.download_as_bytearray)
-    # file_bytes = np.asarray(bytearray(f.read(), dtype=np.uint8))
-    pass
+    # clean temp dir
+    shutil.rmtree('./tmp')
+
+    # Model
+    model = torch.hub.load(
+        './yolov5/',
+        'custom',
+        source='local',
+        path='/models/yolov5s.pt',
+        force_reload=True,
+    )
+    # model = torch.hub.load("ultralytics/yolov5", "yolov5s")
+
+    results = model(photo_weld, size=640)
+    results.save(save_dir='./tmp')
+    return f'./tmp/{results.files[0]}'
 
 
-def get_predict(update, context):
+def get_predict(update, context, photo=True):
     chat = update.effective_chat
     try:
-        photo_weld = context.bot.get_file(update.message.photo[-1].file_id)
+        if photo:
+            photo_weld = context.bot.get_file(update.message.photo[-1].file_id)
+        else:
+            photo_weld = context.bot.get_file(update.message.document)
 
-        context.bot.send_photo(chat.id, get_image_predict(photo_weld))
+        inference_file = open(get_image_predict(photo_weld.file_path), 'rb')
+
+        context.bot.send_photo(chat.id, inference_file, 'Мой вердикт')
+        inference_file.close()
     except Exception as error:
         context.bot.send_message(
             chat_id=chat.id,
@@ -57,13 +58,16 @@ def get_predict(update, context):
 
 
 def only_photo(update, context):
-    chat = update.effective_chat
-    name = update.message.chat.first_name
+    if update.message.text != None and len(update.message.text) > 0:
+        chat = update.effective_chat
+        name = update.message.chat.first_name
 
-    context.bot.send_message(
-        chat_id=chat.id,
-        text=f'Ожидаю только фото сварного шва для детекции дефектов! Большего, я пока не могу, {name}!',
-    )
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=f'Ожидаю только фото сварного шва для детекции дефектов! Большего, я пока не могу, {name}!',
+        )
+    elif update.message.document != None:
+        get_predict(update, context, photo=False)
 
 
 def feedback(update, context):
@@ -90,10 +94,10 @@ def wake_up(update, context):
 def main():
     updater = Updater(token=BOT_TOKEN)
 
-    # updater.dispatcher.add_handler(MessageHandler(~Filters.photo, only_photo))
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, get_predict))
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
     updater.dispatcher.add_handler(CommandHandler('feedback', feedback))
+    updater.dispatcher.add_handler(MessageHandler(~Filters.photo, only_photo))
 
     updater.start_polling()
     updater.idle()
